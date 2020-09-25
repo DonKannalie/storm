e25d665e2cc9da6b78d7a89db60fd11f1d00be86
DonKannalie <19978132+DonKannalie@users.noreply.github.com>
2020-09-25 12:42:06 +0200

    Update utils.py

--- storm/utils.py
+++ storm/utils.py
@@ -24,22 +24,20 @@ COLOR_CODES = [
 
 
 def get_formatted_message(message, format_type):
+    # required for CLI test suite. see tests.py
+    if 'TESTMODE' in os.environ and not isinstance(message, ValueError):
+        for color_code in COLOR_CODES:
+            message = message.replace(color_code, "")
 
-    #    # required for CLI test suite. see tests.py
-    #    if 'TESTMODE' in os.environ and not isinstance(message, ValueError):
-    #        for color_code in COLOR_CODES:
-    #            message = message.replace(color_code, "")
-    #
-    #        return "{0} {1}".format(format_type, message)
-    #
-    #    format_typed = fixed_width(format_type, 8)
-    #    all_message = ""
-    #    message = " %s" % message
-    #
-    #    if format_type == 'error':
-    #        all_message = colored(format_typed, 'white', 'on_red')
-    #    if format_type == 'success':
-    #        all_message = colored(format_typed, 'white', 'on_green')
+        return "{0} {1}".format(format_type, message)
 
-    #return all_message + message
-    return message
+    format_typed = fixed_width(format_type, 8)
+    all_message = ""
+    message = " %s" % message
+
+    if format_type == 'error':
+        all_message = colored(format_typed, 'white', 'on_red')
+    if format_type == 'success':
+        all_message = colored(format_typed, 'white', 'on_green')
+
+    return all_message + message
