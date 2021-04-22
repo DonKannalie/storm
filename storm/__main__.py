f7ee3541521d9d81e8767ec8b101a88debba9cc4
DonKannalie <19978132+DonKannalie@users.noreply.github.com>
2021-04-22 16:33:25 +0200

    dev

--- storm/__main__.py
+++ storm/__main__.py
@@ -416,10 +416,12 @@ def copy_ids(name, config=None):
 @command('ping')
 @arg('glob', action='store_true', default=False)
 @arg('n', type=int, default=1)
-def ping_host(name, n, config=None, glob=False):
+def ping_host(name, n=None, config=None, glob=False):
     """
     ping host
     """
+    if n is None:
+        n = 1
     storm_ = get_storm_instance(config)
     ips = storm_.get_hostname(name, glob=glob)
     if ips:
