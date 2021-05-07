7175b8ec0b0a508e63f0ef51693791a5188f5de2
DonKannalie <ohnemir@gmx.de>
2021-05-07 22:18:19 +0200

    dev

--- storm/parsers/storm_config_parser.py
+++ storm/parsers/storm_config_parser.py
@@ -10,11 +10,22 @@ from storm.utils import get_formatted_message
 from storm.defaults import CONFIG_DATA
 import json
 
+config_root = Path.home().joinpath(".config/stormssh")
+config_file = config_root.joinpath("config")
+
+
+def create_config():
+    print("StormSSH: Config file not found!")
+    config_root.mkdir(exist_ok=True)
+    config_file.touch()
+    if config_file.exists():
+        with open(config_file, 'w') as cf:
+            json.dump(CONFIG_DATA, cf, indent=4)
+        if config_file.exists():
+            print(get_formatted_message("StormSSH: Config file created: %s." % config_file, 'success'))
+
 
 def get_storm_config():
-    config_root = Path.home().joinpath(".config/stormssh")
-    config_file = config_root.joinpath("config")
-
     if config_file.exists():
         try:
             config_data = json.loads(open(config_file).read())
@@ -23,13 +34,6 @@ def get_storm_config():
         except Exception as error:
             pass
     else:
-        print("StormSSH: Config file not found!")
-        config_root.mkdir(exist_ok=True)
-        config_file.touch()
-        if config_file.exists():
-            with open(config_file, 'w') as cf:
-                json.dump(CONFIG_DATA, cf, indent=4)
-            if config_file.exists():
-                print(get_formatted_message("StormSSH: Config file created: %s." % config_file, 'success'))
+        create_config()
 
     return {}
