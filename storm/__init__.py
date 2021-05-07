75e51e8caf896afed8cc3f333dd9d7cd6a94df51
DonKannalie <ohnemir@gmx.de>
2021-05-07 21:25:13 +0200

    dev

--- storm/__init__.py
+++ storm/__init__.py
@@ -9,7 +9,7 @@ from shutil import copyfile
 from .parsers.ssh_config_parser import ConfigParser
 from .defaults import get_default
 
-__version__ = '0.7.26'
+__version__ = '0.7.28'
 
 ERRORS = {
     "already_in": "{0} is already in your sshconfig. "
