4e5499c8d6de539f9b8fbf99c695625d098fa5eb
DonKannalie <ohnemir@gmx.de>
2021-05-07 21:24:01 +0200

    dev

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import sys
 
 from setuptools import setup, find_packages
+from storm import __version__
 
 setup(
     name='stormssh',
-    version='0.7.0',
+    version=__version__,
     packages=find_packages(),
     package_data={'storm': ['templates/*.html', 'static/css/*.css',
                             'static/css/themes/storm/*.css', 'static/css/themes/storm/img/*.png',
