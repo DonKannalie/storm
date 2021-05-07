64b2732756caac7033d2694276e2ef2687736077
DonKannalie <ohnemir@gmx.de>
2021-05-07 22:45:12 +0200

    Revert "dev"
    This reverts commit 7e953600

--- storm/parsers/storm_config_parser.py
+++ storm/parsers/storm_config_parser.py
@@ -6,11 +6,20 @@ from storm.defaults import CONFIG_DATA
 import json
 import sys
 
+config_root = Path.home().joinpath(".config/stormssh")
+config_file = config_root.joinpath("config")
+
+
+def create_config():
+    print(get_formatted_message("StormSSH: Config file not found!", 'error'), file=sys.stderr)
+    config_root.mkdir(exist_ok=True)
+    with open(config_file, 'w') as cf:
+        json.dump(CONFIG_DATA, cf, indent=4)
+    if config_file.exists():
+        print(get_formatted_message("StormSSH: Config file created: %s." % config_file, 'success'), file=sys.stderr)
+
 
 def get_storm_config():
-    config_root = Path.home().joinpath(".config/stormssh")
-    config_file = config_root.joinpath("config")
-
     if config_file.exists():
         try:
             config_data = json.loads(open(config_file).read())
@@ -18,13 +27,6 @@ def get_storm_config():
         except Exception as error:
             print(get_formatted_message("StormSSH: Failure to load %s." % config_file, 'error'), file=sys.stderr)
     else:
-        print("StormSSH: Config file not found!")
-        config_root.mkdir(exist_ok=True)
-        config_file.touch()
-        if config_file.exists():
-            with open(config_file, 'w') as cf:
-                json.dump(CONFIG_DATA, cf, indent=4)
-            if config_file.exists():
-                print(get_formatted_message("StormSSH: Config file created: %s." % config_file, 'success'))
+        create_config()
 
     return {}
