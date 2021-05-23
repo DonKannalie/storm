d13e284bcd2acbe46f9afc20fef2ef6b1421f217
DonKannalie <ohnemir@gmx.de>
2021-05-23 21:22:27 +0200

    added user, host, port colors & padding to list items; actually not working

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ setup(
         "argparse" if sys.version_info[:2] < (2, 7) else None,
         "six",
         "iterfzf",
-        "ping3"
+        "ping3",
+        "wakeonlan"
     ])),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
